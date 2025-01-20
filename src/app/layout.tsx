af0b07f5a492e53983871843e3148cb858b74ada
Zekarias Mesfin <zekariasmesfin100@gmail.com>
2025-01-20 10:38:36 +0300

    second commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { WixClientContextProvider } from "@/context/wixContext";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Vertexia E-Commerce",
+  description: "Your goto onlne gift shop",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <WixClientContextProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </WixClientContextProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/types/cart.ts
@@ -0,0 +1,3 @@
+export type Cart = {
+    subtotal: number;
+}
